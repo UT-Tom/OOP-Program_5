8d3795e8317280473609894dd167f9d3f04105ea
JakobLopez <thejakoblopez@gmail.com>
2017-11-25 14:48:27 -0600

    Update teamQueue.cpp

--- teamQueue.cpp
+++ teamQueue.cpp
@@ -1,56 +1,23 @@
-//This is just a basic queue.Still needs modification to
-//enqueue
+//teamQueue.cpp
 
+//This is a linked-list based queue that inserts data values next 
+//to their "team" members.
 #include"teamQueue.h"
 
 
 //Default constructor
 teamQueue::teamQueue() 
 {
-	Front = Rear = NULL;
+	Front = Rear = curr = NULL;
 }
 
 /**
- *  MethodName: Empty()
- *  Description: Checks if queue is empty
- *  Parameters: n/a
- *  Returns: bool - empty or not
+*  MethodName: dequeue()
+*  Description: Removes front item in queue
+*  Parameters: n/a
+*  Returns: int - number at the front of queue
 **/
-bool teamQueue:: Empty() 
-{
-	return Front == NULL;
-}
-
-/**
- *  MethodName: enqueue()
- *  Description: Adds an item to queue next to its team member
- *  Parameters: int val - a number
- *  Returns: void
-**/
-void teamQueue:: enqueue(int val)
-{
-	Node* Temp = new Node;
-	Temp->Data = val;
-	Temp->Next = NULL;
-
-	if (Empty())
-	{
-		Front = Rear = Temp;
-	}
-	else
-	{
-		Rear->Next = Temp;
-		Rear = Temp;
-	}
-}
-/**
- *  MethodName: dequeue()
- *  Description: Removes front item in queue
- *  Parameters: n/a
- *  Returns: int - number at the front of queue
-**/
-
-int teamQueue:: dequeue()
+int teamQueue::dequeue()
 {
 	int TempData = Front->Data;
 	Node* TempNodePtr = Front;
@@ -58,6 +25,69 @@ int teamQueue:: dequeue()
 	delete TempNodePtr;
 	return TempData;
 }
+
+/**
+*  MethodName: Empty()
+*  Description: Checks if queue is empty
+*  Parameters: n/a
+*  Returns: bool - empty or not
+**/
+bool teamQueue:: Empty() 
+{
+	return Front == NULL;
+}
+
+/**
+*  MethodName: enqueue()
+*  Description: Adds an item to queue next to its team member
+*  Parameters: int val - a number
+*			   int team - team number
+*  Returns: void
+**/
+void teamQueue:: enqueue(int val,int team)
+{
+	Node* Temp = new Node;
+	Temp->Data = val;
+	Temp->ID = team;
+	Temp->Next = NULL;
+
+	if (Empty())
+	{
+		Front = Rear = curr = Temp;
+	}
+	else
+	{
+		
+		while (Temp->ID != curr->ID && curr->Next != NULL)
+		{
+				curr = curr->Next;			
+		}
+		if (curr->ID == Temp->ID)
+		{
+			Node *prev = curr;
+			curr = curr->Next;
+
+			prev->Next = Temp;
+			Temp->Next = curr;
+
+			if (Rear->Next != NULL)
+			{
+				while (Rear->Next != NULL)
+				{
+					Rear = Rear->Next;
+				}
+			}
+		}
+		else
+		{
+			Rear->Next = Temp;
+			Rear = Temp;
+		}
+		curr = Front;
+	}
+}
+
+
 
 //Prints out queue
 void teamQueue:: Print()
@@ -69,4 +99,10 @@ void teamQueue:: Print()
 		Temp = Temp->Next;
 	}
 	cout << endl;
+}
+
+//Destructor
+teamQueue::~teamQueue()
+{
+	Front = Rear = curr = NULL;
 }
