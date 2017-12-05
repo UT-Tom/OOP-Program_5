fe5ab798229293af471f5ee40658108b95a0f349
JakobLopez <thejakoblopez@gmail.com>
2017-12-04 18:28:24 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 /**
- * @Name: Jakob Lopez && Tom Johnson
- * @Description:
- *    This program implements a list-based queue to solve a team queue
- *		problem. It also demonstrates our ability to work with templated classes.
- * @Date:
- *    12/03/17
- * @Instructor:
- *    Dr. Stringfellow
- */
+* @Name: Jakob Lopez && Tom Johnson
+* @Description:
+*    This program implements a list-based queue to solve a team queue
+*		problem. It also demonstrates our ability to work with templated classes.
+* @Date:
+*    12/03/17
+* @Instructor:
+*    Dr. Stringfellow
+*/
 
 #include<iostream>
 #include<fstream>
@@ -17,15 +17,14 @@
 using namespace std;
 
 //function prototypes
-void closeFiles(fstream&, fstream&);
-void freeArray(int**&,int);
-int getTeamNum(int**&,int,int);
-void initTeams(fstream&,int**&,int&);
-void initQueue(fstream&,fstream&, teamQueue<int>*&,int**&,int);
-void openFiles(fstream&, fstream&);
-void printHeading(fstream&,string);
-void printBorder(fstream&,bool);
-void printCaseNumber(fstream&, int);
+void closeFiles(ifstream&, ofstream&);
+void freeArray(int**&, int);
+int getTeamNum(int**&, int, int);
+void initTeams(ifstream&, int**&, int&);
+void initQueue(ifstream&, ofstream&, teamQueue<int>*&, int**&, int);
+void openFiles(ifstream&, ofstream&);
+void printBorder(ofstream&);
+void printCaseNumber(ofstream&, int);
 
 int main()
 {
@@ -33,7 +32,8 @@ int main()
 	int scenario = 1;
 
 	//open files
-	fstream infile, outfile;
+	ifstream infile;
+	ofstream outfile;
 	openFiles(infile, outfile);
 
 	//Pointer to pointer of teams
@@ -46,19 +46,19 @@ int main()
 	//Loops until t is 0
 	while (t != 0)
 	{
+		printBorder(outfile);
+
 		//Initializes teamQueue
 		teamQueue<int> *Q = new teamQueue<int>();
 
 		//Prints Scenario #
-		printBorder(outfile,false);
-			printCaseNumber(outfile, scenario);
-		printBorder(outfile,false);
+		printCaseNumber(outfile, scenario);
 
 		//Updates scenario number
 		scenario++;
 
 		//Adds or removes items to teamQueue
-		initQueue(infile,outfile, Q, team, t);
+		initQueue(infile, outfile, Q, team, t);
 
 		//Frees memory of team
 		freeArray(team, t);
@@ -76,34 +76,34 @@ int main()
 }
 
 /**
- * @FunctionName: closeFiles
- * @Description:
- *    closes opened files
- * @Params:
- *    ifstream &ifile - allows to read from file
- *    ofstream &ofile - allows to write to file
- * @Returns:
- *    void
- */
+* @FunctionName: closeFiles
+* @Description:
+*    closes opened files
+* @Params:
+*    ifstream &ifile - allows to read from file
+*    ofstream &ofile - allows to write to file
+* @Returns:
+*    void
+*/
 
-void closeFiles(fstream &ifile, fstream &ofile)
+void closeFiles(ifstream &ifile, ofstream &ofile)
 {
 	ifile.close();
 	ofile.close();
 }
 
 /**
- * @FunctionName: freeArray
- * @Description:
- *    Frees memory in 2D array
- * @Params:
- *    int **&team - refernce to 2D array
- *	 int row - number of rows
- * @Returns:
- *    void
- */
+* @FunctionName: freeArray
+* @Description:
+*    Frees memory in 2D array
+* @Params:
+*    int **&team - refernce to 2D array
+*	 int row - number of rows
+* @Returns:
+*    void
+*/
 
-void freeArray(int **&team,int row)
+void freeArray(int **&team, int row)
 {
 	for (int i = 0; i < row; i++)
 	{
@@ -114,18 +114,18 @@ void freeArray(int **&team,int row)
 
 
 /**
- * @FunctionName: getTeamNum
- * @Description:
- *    Searches through 2D array and finds team number of item
- * @Params:
- *    int **&team - refernce to 2D array
- *	 int target - item being searched for
- *    int t - Number of teams
- * @Returns:
- *    int - row number in array if found,otherwise -1
- */
+* @FunctionName: getTeamNum
+* @Description:
+*    Searches through 2D array and finds team number of item
+* @Params:
+*    int **&team - refernce to 2D array
+*	 int target - item being searched for
+*    int t - Number of teams
+* @Returns:
+*    int - row number in array if found,otherwise -1
+*/
 
-int getTeamNum(int **&team,int target,int t )
+int getTeamNum(int **&team, int target, int t)
 {
 
 	for (int i = 0; i < t; i++)
@@ -144,18 +144,18 @@ int getTeamNum(int **&team,int target,int t )
 }
 
 /**
- * @FunctionName: initTeams
- * @Description:
- *    Adds items to a 2D array
- * @Params:
- *    ifstream &ifile - allows to read from file
- *    int **&team - refernce to 2D array
- *    int &t - Reference to the number of teams
- * @Returns:
- *    void
- */
+* @FunctionName: initTeams
+* @Description:
+*    Adds items to a 2D array
+* @Params:
+*    ifstream &ifile - allows to read from file
+*    int **&team - refernce to 2D array
+*    int &t - Reference to the number of teams
+* @Returns:
+*    void
+*/
 
-void initTeams(fstream &ifile,int **&team,int &t)
+void initTeams(ifstream &ifile, int **&team, int &t)
 {
 	int person;
 	int tMembers;
@@ -187,19 +187,19 @@ void initTeams(fstream &ifile,int **&team,int &t)
 }
 
 /**
- * @FunctionName: initQueue
- * @Description:
- *    Adds or removes item onto/from queue
- * @Params:
- *    ifstream &ifile - allows to read from file
- *    teamQueue *&Q - pointer to instance of teamQueue
- *    int **&team - reference to 2D array
- *    int t - Number of teams
- * @Returns:
- *    void
- */
+* @FunctionName: initQueue
+* @Description:
+*    Adds or removes item onto/from queue
+* @Params:
+*    ifstream &ifile - allows to read from file
+*    teamQueue *&Q - pointer to instance of teamQueue
+*    int **&team - reference to 2D array
+*    int t - Number of teams
+* @Returns:
+*    void
+*/
 
-void initQueue(fstream &ifile,fstream &ofile, teamQueue<int> *&Q,int **&team,int t )
+void initQueue(ifstream &ifile, ofstream &ofile, teamQueue<int> *&Q, int **&team, int t)
 {
 	string method;
 	int member;
@@ -217,25 +217,29 @@ void initQueue(fstream &ifile,fstream &ofile, teamQueue<int> *&Q,int **&team,int
 			ifile >> member;
 
 			//Gets team number
-			int id = getTeamNum(team, member,t);
+			int id = getTeamNum(team, member, t);
 
 			//Puts item in queue
 			Q->enqueue(member, id);
 
-			ofile<<Q->Print();
+			ofile << Q->Print();
 		}
 		else
 		{
-			//If Q is not empty
-			if (!Q->Empty())
+			//Exception
+			try
 			{
+				//If queue is empty
+				if (Q->Empty())
+					throw string("Q is empty");
 
 				int dequeued = Q->dequeue();
 				ofile << "Dequeued: " << dequeued << endl;
 			}
-			//If queue is empty
-			else
-				ofile << "Q is empty" << endl;
+			catch (string message)
+			{
+				ofile << message << endl;
+			}	
 		}
 		//Reads method
 		ifile >> method;
@@ -248,121 +252,59 @@ void initQueue(fstream &ifile,fstream &ofile, teamQueue<int> *&Q,int **&team,int
 	if (Q->Empty())
 		ofile << "There is nothing in the queue";
 	else
-		ofile<<Q->Print();
+		ofile << Q->Print();
 
 	ofile << endl;
 }
 
 /**
- * @FunctionName: openFiles
- * @Description:
- *    opens files
- * @Params:
- *    ifstream &ifile - allows to read from file
- *    ofstream &ofile - allows to write to file
- * @Returns:
- *    void
- */
+* @FunctionName: openFiles
+* @Description:
+*    opens files
+* @Params:
+*    ifstream &ifile - allows to read from file
+*    ofstream &ofile - allows to write to file
+* @Returns:
+*    void
+*/
 
-void openFiles(fstream &ifile, fstream &ofile)
+void openFiles(ifstream &ifile, ofstream &ofile)
 {
+	string i = "input.txt";
+	ifile.open(i);
 
-	#ifdef __linux__
-		system("clear");
-	#endif
-
-	int choice;
-	string i, o;
-
-	cout << "Choose input file:\n1. input.txt\n2. Custom file\n";
-	cin >> choice;
-
-	if(choice != 1 && choice != 2){
-		cout << "Not an option, try again.\n";
-		openFiles(ifile,ofile);
-	} else if(choice == 1) {
-		i = "input.txt";
-	} else {
-		cout << "Name of file: ";
-		cin >> i;
-	}
-
-	#ifdef _WIN32
-		system("cls");
-	#elif __linux__
-		system("clear");
-	#endif
-
-	cout << "Choose output file:\n1. output.txt\n2. Custom file\n";
-	cin >> choice;
-
-	if(choice != 1 && choice != 2){
-		cout << "Not an option, try again.\n";
-		openFiles(ifile,ofile);
-	} else if(choice == 1) {
-		o = "output.txt";
-	} else {
-		cout << "Name of file: ";
-		cin >> o;
-	}
-
-	ifile.open(i, ios::in);
-	ofile.open(o, ios::out);
-
-	if(!ifile.is_open() || !ofile.is_open())
-		cout << "One or both of the files couldn't open. Check the filesnames!\n\n";
-
-	printHeading(ofile,i);
+	string o = "output.txt";
+	ofile.open(o);
 }
 
 /**
- * @FunctionName: printHeading
- * @Description:
- *    Prints out ouput heading
- * @Params:
- *    ofstream &ofile - allows to write to file
- * @Returns:
- *    void
- */
+* @FunctionName: printBorder
+* @Description:
+*    prints "*" as a border
+* @Params:
+*    ofstream &ofile - allows to write to file
+* @Returns:
+*    void
+*/
 
-void printHeading(fstream &ofile, string input)
+void printBorder(ofstream &ofile)
 {
-	printBorder(ofile,true);
-		ofile << "Output run for " << input << "\n";
-	printBorder(ofile,true);
-		ofile << "\n";
+	ofile << "\n**************************************************************\n";
+	ofile << endl;
 }
 
 /**
- * @FunctionName: printBorder
- * @Description:
- *    prints "*" as a border
- * @Params:
- *    ofstream &ofile - allows to write to file
- * @Returns:
- *    void
- */
+* @FunctionName: printCaseNumber
+* @Description:
+*    prints out case number
+* @Params:
+*    ofstream &ofile - allows to write to file
+*    int number - case number
+* @Returns:
+*    void
+*/
 
- void printBorder(fstream &ofile, bool type)
- {
- 	string s;
- 	for(int i = 0; i < 62; i++)
- 		s += (type?"=":"*");
- 	ofile << s << "\n";
- }
-
-/**
- * @FunctionName: printCaseNumber
- * @Description:
- *    prints out case number
- * @Params:
- *    ofstream &ofile - allows to write to file
- *    int number - case number
- * @Returns:
- *    void
- */
-
-void printCaseNumber(fstream &ofile, int number)
+void printCaseNumber(ofstream &ofile, int number)
 {
-	ofile << "Scenario #" << number << "\n";
+	ofile << "Scenario #" << number << endl;
 }
